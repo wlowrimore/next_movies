c9abca47d4cb9f15a643f3874b64434258420b1c
William Lowrimore <wlowrimore@gmail.com>
2024-02-11 22:01:27 -0600

    added dynamic nav link rendering depending on if search is performed.

--- src/app/components/HeaderNav.jsx
+++ src/app/components/HeaderNav.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
+'use client'
+
+import { useSearch } from '@/searchContext';
 import MovieGenre from './ui/selectDropdowns/MovieGenre';
 import TVGenre from './ui/selectDropdowns/TVGenre';
 import SearchBar from './forms/SearchBar';
 import Link from 'next/link';
 
 const HeaderNav = () => {
-
+  const { searchQuery, updateSearchQuery } = useSearch();
   return (
     <nav className='bg-neutral-800 w-full p-4 flex justify-around fixed top-0 z-10 shadow-lg shadow-neutral-800'>
       <Link href='/' className='text-amber-400 text-4xl cursor-pointer hover:bg-neutral-700 px-3 py-1 rounded-xl transition duration-300'><h1>Next Movies</h1></Link>
       <div className='flex items-center gap-8'>
-        <MovieGenre />
-        <TVGenre />
+        {!searchQuery ?
+          <>
+            <MovieGenre />
+            <TVGenre />
+          </>
+          : null}
+
       </div>
       <SearchBar />
     </nav>
